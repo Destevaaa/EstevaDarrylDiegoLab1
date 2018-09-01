1d2788ba05bce0b930338990f7635b5f6cb19078
Destevaaa <2015081193@ust-ics.mygbiz.com>
2018-09-01 12:50:15 +0800

    Initial commit

---
+++ app/src/main/java/darryl/esteva/com/estevadarryllab1/Activity2_Esteva.java
@@ -0,0 +1,13 @@
+package darryl.esteva.com.estevadarryllab1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Activity2_Esteva extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_activity2__esteva);
+    }
+}
